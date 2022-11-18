f9706ca2e4628ca20bf1327e0869ae1a269eb555
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-11-18 10:58:40 -0300

    Propriedades de exemplo

--- RecursosCSharp11/RecursosCSharp11/RawStringLiteral.cs
+++ RecursosCSharp11/RecursosCSharp11/RawStringLiteral.cs
@@ -2,11 +2,28 @@
 {
     public class RawStringLiteral
     {
+        public RawStringLiteral()
+        {
+            this.StringLiteralExemplo3 = $$"""
+                                                 {
+                                                    "id" : "{{Id}}",
+                                                    "nome" : "{{Nome}}",
+                                                    "pais" : "{{Pais}}"
+                                                 }
+                                              """;
+        }
+
+        public int Id = 10;
+
+        public string Nome = "Rafael";
+
+        public string Pais = "Brasil";
+        
         public string StringLiteralExemplo1 = """ Isto é uma "String literal" """;
 
         public string StringLiteralExemplo2 = $$""" {10 + 20} = {{10 + 20}} """;
 
-        public string StringLiteralExemplo3 = $$""" Isto é uma "String literal" """;
+        public string StringLiteralExemplo3 = string.Empty;
 
         public string StringLiteralExemplo4 = """
                                                  {
