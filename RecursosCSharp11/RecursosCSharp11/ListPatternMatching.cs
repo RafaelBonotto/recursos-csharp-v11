9efce9542841a2e287c810210187cc66bb456d18
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-11-18 13:33:36 -0300

    Segundo teste

--- RecursosCSharp11/RecursosCSharp11/ListPatternMatching.cs
+++ RecursosCSharp11/RecursosCSharp11/ListPatternMatching.cs
@@ -8,7 +8,16 @@
             int[] numeros = { 1, 2, 3};
             result = numeros is [1, _, _];
 
-            return result; // Resultado = true (Primeira posição numero 1)
+            return result; // Resultado = true (Primeira posição número 1 encontrada)
+        }
+
+        public bool VerificaSegundaPosicao()
+        {
+            bool result = false;
+            int[] numeros = { 1, 2, 3 };
+            result = numeros is [_, 3, _];
+
+            return result; // Resultado = false (Segunda posição para número 3 não encontrada)
         }
     }
 }
