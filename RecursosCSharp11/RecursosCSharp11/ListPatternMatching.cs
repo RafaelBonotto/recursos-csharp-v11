a1beaac750ff20135d830ff1bf0adce9a9d9d6bf
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-11-18 13:26:34 -0300

    Método teste 1

--- RecursosCSharp11/RecursosCSharp11/ListPatternMatching.cs
+++ RecursosCSharp11/RecursosCSharp11/ListPatternMatching.cs
@@ -2,6 +2,13 @@
 {
     public class ListPatternMatching
     {
+        public bool VerificaPrimeiraPosicao()
+        {
+            bool result = false;
+            int[] numeros = { 1, 2, 3};
+            result = numeros is [1, _, _];
 
+            return result; // Resultado = true (Primeira posição numero 1)
+        }
     }
 }
